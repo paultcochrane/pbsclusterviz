a48aa3892511e0f73eafe98cbd03cbdaa99277b9
Paul Cochrane <paultcochrane@users.sourceforge.net>
2009-10-11 20:23:46 +0000

    Added a setup.py file for the distutils package to organise packaging, installation and distribution

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@
 from distutils.core import setup
 
 a=setup(name="pbsclusterviz",
-      version="0.1-alpha-1",
-      description="PBS Cluster Viz",
+      version="0.1",
+      description="PBS Cluster Visualisation",
       author="Paul Cochrane",
-      author_email="paultcochrane@gmail.com",
+      author_email="paultcochrane@users.sourceforge.net",
+      maintainer="Paul Cochrane",
+      maintainer_email="paultcochrane@users.sourceforge.net",
       url="http://pbsclusterviz.sourceforge.net",
-      packages=['pbsclusterviz',
-      ],
+      license="GPL",
+      keywords="PBS Torque Maui Cluster HPC Visualisation",
+      platforms="OS Independent",
+      packages=['pbs'],
+      scripts=['bin/cluster_job_status bin/cluster_load_status'],
 )
+
 
