bfa7d1b97716417bc3d3bb6050ef501cff85afd5
Paul Cochrane <paultcochrane@users.sourceforge.net>
2012-10-12 12:32:09 +0000

    Added vim coda to setup.py and removed tabs

--- setup.py
+++ setup.py
@@ -15,12 +15,14 @@ a=setup(name="pbsclusterviz",
       platforms="OS Independent",
       packages=['pbsclusterviz'],
       scripts=[
-		'bin/cluster_status',
-		'bin/gen_nodes_file',
-		],
+                'bin/cluster_status',
+                'bin/gen_nodes_file',
+                ],
       data_files=[('/etc/pbsclusterviz.d',
-		    [
-		    'etc/pbsclusterviz.d/nodes',
-		    'etc/pbsclusterviz.d/clusterviz.conf'
-		    ])],
+                    [
+                    'etc/pbsclusterviz.d/nodes',
+                    'etc/pbsclusterviz.d/clusterviz.conf'
+                    ])],
 )
+
+# vim: expandtab shiftwidth=4:
